b86f92dcb6e1da6e525baa1d5cc4194bbd5fefa2
hannahmuermann <112498721+hannahmuermann@users.noreply.github.com>
2022-09-14 23:56:08 -0400

    Part 1 - Commit 2: Initial Version of adventure.py

---
+++ GAME/adventure.py
@@ -0,0 +1,52 @@
+def main():
+    print("Welcome to Hannah's Haunted House! You will travel through several rooms and collect many objects in order to escape this spooky home.")
+
+    begin = input("Press any any to begin: ")
+    
+    foyer = "This is an old foyer in front of a locked door. The lights are dim."
+    print(foyer)
+    cont = input("Press any key to continue: ")
+    
+    hallway = "The hallway is long, and lacks decorations. The lights are bright. Doors line each wall."
+    print(hallway)
+    cont = input("Press any key to continue: ")
+    
+    bathroom = "The bathroom is lit by a dim candle. There are cobwebs in the corner."
+    print(bathroom)
+    cont = input("Press any key to continue: ")
+    
+    bedroom = "The bedroom is a tight space with a queen size bed. The only light is coming from the hallway. There is a locked window."
+    print(bedroom)
+    cont = input("Press any key to continue: ")
+
+    nursery = "The nursery is completely empty. There is a single floorboard coming up. The window is locked."
+    print(nursery)
+    cont = input("Press any key to continue: ")
+
+    home_office = "The home office has a small desk and a rolling chair. The window is locked."
+    print(home_office)
+    cont = input("Press any key to continue: ")
+
+    living_room = "The living room has a large old couch, and a TV playing at a low volume. The lights are dim. There are two locked windows."
+    print(living_room)
+    cont = input("Press any key to continue: ")
+
+    kitchen = "The kitchen has a stove, oven, fridge, and table with two chairs. The only light is coming from the hallway. The window is locked."
+    print(kitchen)
+    cont = input("Press any key to continue: ")
+
+    storage_room = "The storage room has 6 shelves, but only one box on them. The only light is coming from the hallway. There are no windows."
+    print(storage_room)
+    cont = input("Press any key to continue: ")
+
+    guest_room = "The guest room has a single bed and a nightstand. The lights are dim. There is no window."
+    print(guest_room)
+    cont = input("Press any key to continue: ")
+
+    home_gym = "The home gym only contains a treadmill and set of weights. The lights are dim. The window is locked."
+    print(home_gym)
+    cont = input("Press any key to continue: ")
+
+    print("Thank you, goodbye!")
+
+main()
