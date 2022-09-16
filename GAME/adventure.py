04976c63cbfbcf2962950b374fb24631459b2d82
hannahmuermann <112498721+hannahmuermann@users.noreply.github.com>
2022-09-16 11:43:10 -0400

    Part 1 - Commit 5: More Details

--- GAME/adventure.py
+++ GAME/adventure.py
@@ -4,7 +4,7 @@ def main():
     intro = "Welcome to Hannah's Haunted House! You will travel through several rooms and collect many objects in order to escape this spooky home."
     goodbye = "Thank you for playing, goodbye!"
 
-    # All rooms
+    # All Rooms
     foyer = "This is an old foyer in front of a locked door. The lights are dim."
     hallway = "The hallway is long, and lacks decorations. The lights are bright. Doors line each wall."
     bathroom = "The bathroom is lit by a dim candle. There are cobwebs in the corner."
@@ -18,40 +18,75 @@ def main():
     home_gym = "The home gym only contains a treadmill and set of weights. The lights are dim. The window is locked."
     room_counter = 0
 
+    # Room Details
+    foyer_details = "When you look closer, you see a large rug in front of the door."
+    hallway_details = "On the left side of the hallway, there is a large painting of a fire."
+    bathroom_details = "When you look closer, you see a rusted sink and a shower with a ripped curtain."
+    bedroom_details = "When you look under the bed, there is a small shoebox and a key."
+    nursery_details = "When you look under the floorboard, there is a pacifier."
+    home_office_details = "When you open the desk, there is a notepad and pen."
+    living_room_details =  "When you look closer at the TV, it is playing breaking news. The screen has cracks so you cannot see what is playing, and the volume is too quiet to hear it."
+    kitchen_details = "When you open the fridge, there is a water bottle filled with a liquid."
+    storage_room_details = "Inside the box, there is an old photo album."
+    guest_room_details = "When you look inside the nightstand, there is a small box."
+    home_gym_details = "Underneath the treadmill, there is a small box."
+
     # This starts with an intro statement, and then goes through each room by having the user press and key to continue. At the end, a goodbye message is printed.
     print(intro)
     begin = input("Press any any to begin: ")
     print(foyer)
     cont = input("Press any key to continue: ")
+    print(foyer_details)
+    cont = input("Press any key to continue: ")
     room_counter += 1
     print(hallway)
+    cont = input("Press any key to continue: ")
+    print(hallway_details)
     cont = input("Press any key to continue: ")
     room_counter += 1
     print(bathroom)
     cont = input("Press any key to continue: ")
+    print(bathroom_details)
+    cont = input("Press any key to continue: ")
     room_counter += 1
     print(bedroom)
+    cont = input("Press any key to continue: ")
+    print(bedroom_details)
     cont = input("Press any key to continue: ")
     room_counter += 1
     print(nursery)
     cont = input("Press any key to continue: ")
+    print(nursery_details)
+    cont = input("Press any key to continue: ")
     room_counter += 1
     print(home_office)
+    cont = input("Press any key to continue: ")
+    print(home_office_details)
     cont = input("Press any key to continue: ")
     room_counter += 1
     print(living_room)
     cont = input("Press any key to continue: ")
+    print(living_room_details)
+    cont = input("Press any key to continue: ")
     room_counter += 1
     print(kitchen)
+    cont = input("Press any key to continue: ")
+    print(kitchen_details)
     cont = input("Press any key to continue: ")
     room_counter += 1
     print(storage_room)
     cont = input("Press any key to continue: ")
+    print(storage_room_details)
+    cont = input("Press any key to continue: ")
     room_counter += 1
     print(guest_room)
     cont = input("Press any key to continue: ")
+    print(guest_room_details)
+    cont = input("Press any key to continue: ")
     room_counter += 1
     print(home_gym)
+    cont = input("Press any key to continue: ")
+    print(home_gym_details)
     cont = input("Press any key to continue: ")
     room_counter += 1
     print("You visited", room_counter, "rooms.")
